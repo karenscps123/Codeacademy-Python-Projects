8305d3a4d69af15b3d3c1c4d4ed505782f056344
Karen Cheung Kai Ning <119771144+karenscps123@users.noreply.github.com>
2023-01-11 19:05:38 +0800

    Update Sal's Shipping.py

--- 2. Control Flow/Sal's Shipping/Sal's Shipping.py
+++ 2. Control Flow/Sal's Shipping/Sal's Shipping.py
@@ -1,6 +1,10 @@
 # Sal's Shipping
 
-weight = 8.4
+#  user dialogues
+name = input("What is your name?")
+print("Hello,", name, "!")
+answer = input("What is the weight of your package?")
+weight = int(answer)
 cost = 0
 
 # Ground Shipping 
@@ -13,9 +17,12 @@ elif (weight > 6 and weight <= 10):
 else:
   cost = weight * 4.75
 print("Normal ground shipping cost = $", cost + 20)
+normal_g = cost + 20
+
 
 # Ground Shipping premium 
 print("Normal ground shipping premium cost = $ 125.0 ")
+g_prime = 125 
 
 # Drone shipping
 if weight <= 2:
@@ -27,3 +34,15 @@ elif (weight > 6 and weight <= 10):
 else:
   cost = weight * 14.25
 print("Drone shipping cost = $", cost)
+drone = cost 
+
+# finding cheapest
+
+if (normal_g < g_prime) and (normal_g < drone):
+    smallest_num = normal_g
+elif (g_prime < normal_g) and (g_prime < drone):
+    smallest_num = g_prime
+else:
+    smallest_num = drone
+
+print("The cheapest option is : ", smallest_num)
