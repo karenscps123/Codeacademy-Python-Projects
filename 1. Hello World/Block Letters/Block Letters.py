63974a322c48a1265f30e82a1622e13dd1778be5
Karen Cheung Kai Ning <119771144+karenscps123@users.noreply.github.com>
2023-01-11 18:16:45 +0800

    Create Block Letters.py

---
+++ 1. Hello World/Block Letters/Block Letters.py
@@ -0,0 +1,29 @@
+#first_name = 'Karen'
+#last_name = 'Cheung'
+#fun_fact = 'I love my cats'
+print ('K   K')
+print ('K  K ')
+print ('K K  ')
+print ('KK   ')
+print ('K K  ')
+print ('K  K ')
+print ('K   K')
+
+print('''
+K   K
+K  K 
+K K  
+KK   
+K K  
+K  K 
+K   K ''')
+
+print('''
+ CCC 
+C   C
+C
+C
+C
+C   C
+ CCC 
+''')
