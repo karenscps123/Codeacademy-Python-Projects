4a59729b30f82942940bf211090a975795b8bf56
Karen Cheung Kai Ning <119771144+karenscps123@users.noreply.github.com>
2023-01-11 17:56:22 +0800

    Block Letters

---
+++ Block Letters.py
@@ -0,0 +1,21 @@
+# Block Letters 
+
+#first_name = 'Karen'
+#last_name = 'Cheung'
+#fun_fact = 'I love my cats'
+print ('K   K')
+print ('K  K ')
+print ('K K  ')
+print ('KK   ')
+print ('K K  ')
+print ('K  K ')
+print ('K   K')
+
+print('''
+K   K
+K  K 
+K K  
+KK   
+K K  
+K  K 
+K   K ''')
