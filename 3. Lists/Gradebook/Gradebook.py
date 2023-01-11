7a515254448ad291b4573022cb21962e48c8fbbe
Karen Cheung Kai Ning <119771144+karenscps123@users.noreply.github.com>
2023-01-11 19:50:44 +0800

    Create Gradebook.py

---
+++ 3. Lists/Gradebook/Gradebook.py
@@ -0,0 +1,28 @@
+last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["architecture", 65]]
+
+# Your code below: 
+
+subjects = ["physics", "calculus", "poetry", "history"]
+
+grades = [98, 97, 85, 88]
+
+gradebook = [["physics", 98], ["calculus", 97], ["poetry", 85], ["history", 88]]
+
+print(gradebook)
+
+# Add More Subjects:
+gradebook.append(["computer science", 100])
+gradebook.append(["visual arts", 93])
+
+print(gradebook)
+
+# Modify The Gradebook:
+gradebook[-1][-1] = 98
+gradebook[2].remove(85)
+gradebook[2].append("Pass")
+print(gradebook)
+
+#one big gradebook 
+
+full_gradebook = last_semester_gradebook + gradebook
+print(full_gradebook)
